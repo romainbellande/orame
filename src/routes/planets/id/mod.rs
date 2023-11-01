fc18db8f026ca48905293f2439334e8b6681ad65
Romain Bellande <rbellande@wedigital.garden>
2023-11-01 17:15:41 +0100

    feat: window

---
+++ src/routes/planets/id/mod.rs
@@ -0,0 +1,18 @@
+use leptos::*;
+use leptos_router::use_params_map;
+
+mod toolbar;
+use toolbar::Toolbar;
+
+#[component]
+pub fn PageID() -> impl IntoView {
+    let params = use_params_map();
+    let id = move || params.with(|params| params.get("id").cloned().unwrap_or_default());
+
+    view! {
+      <div>
+        { format!("planet {:?}", id()) }
+        <Toolbar />
+      </div>
+    }
+}
