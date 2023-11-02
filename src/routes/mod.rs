783af8ee09aaf8c644b72247dfaab344c435b131
Champii1 <contact@champii.io>
2023-11-02 11:32:06 +0100

    First ticks from the game in the client

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,17 +1,38 @@
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use wasm_bindgen::{prelude::Closure, JsCast};
 
 mod home;
-use home::Home;
-
 mod planets;
+
 use crate::components::{header::Header, sidenav::SideNav};
+
+use home::Home;
+use ogame_core::game::Game;
 use planets::PageID;
+
+fn set_tick_interval(game: RwSignal<Game>) {
+    let cb = Closure::wrap(Box::new(move || {
+        game.update(|game| {
+            game.tick().unwrap();
+        });
+    }) as Box<dyn FnMut()>);
+
+    let window = web_sys::window().unwrap();
+    let _interval_id = window
+        .set_interval_with_callback_and_timeout_and_arguments_0(cb.as_ref().unchecked_ref(), 1_000)
+        .unwrap();
+    cb.forget(); // leak the closure
+}
 
 #[component]
 pub fn AppRouter() -> impl IntoView {
     provide_meta_context();
+
+    let context = create_rw_signal(Game::new());
+    set_tick_interval(context);
+    provide_context(context);
 
     view! {
       <Stylesheet id="leptos" href="/pkg/tailwind.css"/>
