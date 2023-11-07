1695024d14ac935a4321e237510a74aba2017b23
Champii1 <contact@champii.io>
2023-11-07 13:01:13 +0100

    Cleanup

--- server/src/socket.rs
+++ server/src/socket.rs
@@ -67,120 +67,7 @@ async fn fetch_game(user_id: String, conn: &Arc<PrismaClient>) -> Game {
         .unwrap()
         .unwrap();
 
-    println!("GOT USER GAME: {:#?}", user_game);
-
     user_game.into()
-}
-
-async fn save_game(game: Game, conn: &Arc<PrismaClient>) {
-    use prisma_client::{coordinates, planet, user, PrismaClient};
-
-    for (_, planet) in game.planets {
-        conn.planet()
-            .update(
-                planet::id::equals(planet.id.clone()),
-                vec![
-                    planet::build_queue::set(
-                        serde_json::to_string(&planet.build_queue.items).unwrap(),
-                    ),
-                    planet::ship_queue::set(
-                        serde_json::to_string(&planet.ship_queue.items).unwrap(),
-                    ),
-                    planet::last_update::set(planet.last_update as i32),
-                ],
-            )
-            .exec()
-            .await
-            .unwrap();
-
-        conn.resources()
-            .update(
-                resources::planet_id::equals(planet.id.clone()),
-                vec![
-                    resources::metal::set(planet.resources.metal),
-                    resources::crystal::set(planet.resources.crystal),
-                    resources::deuterium::set(planet.resources.deuterium),
-                ],
-            )
-            .exec()
-            .await
-            .unwrap();
-
-        conn.buildings()
-            .update(
-                buildings::planet_id::equals(planet.id.clone()),
-                vec![
-                    buildings::metal::set(
-                        planet.buildings[&ogame_core::building_type::BuildingType::Metal] as i32,
-                    ),
-                    buildings::crystal::set(
-                        planet.buildings[&ogame_core::building_type::BuildingType::Crystal] as i32,
-                    ),
-                    buildings::deuterium::set(
-                        planet.buildings[&ogame_core::building_type::BuildingType::Deuterium]
-                            as i32,
-                    ),
-                    buildings::shipyard::set(
-                        planet.buildings[&ogame_core::building_type::BuildingType::Shipyard] as i32,
-                    ),
-                ],
-            )
-            .exec()
-            .await
-            .unwrap();
-
-        conn.ships()
-            .update(
-                ships::planet_id::equals(planet.id.clone()),
-                vec![
-                    ships::small_cargo::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::SmallCargo] as i32,
-                    ),
-                    ships::large_cargo::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::LargeCargo] as i32,
-                    ),
-                    ships::colony_ship::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::ColonyShip] as i32,
-                    ),
-                    ships::recycler::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::Recycler] as i32,
-                    ),
-                    ships::espionage_probe::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::EspionageProbe] as i32,
-                    ),
-                    ships::solar_satellite::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::SolarSatellite] as i32,
-                    ),
-                    ships::light_fighter::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::LightFighter] as i32,
-                    ),
-                    ships::heavy_fighter::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::HeavyFighter] as i32,
-                    ),
-                    ships::cruiser::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::Cruiser] as i32,
-                    ),
-                    ships::battleship::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::Battleship] as i32,
-                    ),
-                    ships::bomber::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::Bomber] as i32,
-                    ),
-                    ships::destroyer::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::Destroyer] as i32,
-                    ),
-                    ships::battlecruiser::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::Battlecruiser] as i32,
-                    ),
-                    ships::deathstar::set(
-                        planet.ships.ships[&ogame_core::ship_type::ShipType::Deathstar] as i32,
-                    ),
-                ],
-            )
-            .exec()
-            .await
-            .unwrap();
-    }
 }
 
 async fn handle_socket<P: Serialize + DeserializeOwned + Debug + 'static>(
@@ -194,10 +81,11 @@ async fn handle_socket<P: Serialize + DeserializeOwned + Debug + 'static>(
     {
         let mut game = fetch_game(user_id.clone(), &conn).await;
         game.tick().unwrap();
-        println!("Game fetched: {:#?}", game);
 
-        let game_packet = protocol_to_bytes(Protocol::Game(game));
+        let game_packet = protocol_to_bytes(Protocol::Game(game.clone()));
         tx.send(game_packet.into()).await.unwrap();
+
+        save_game(game, &conn).await;
     }
 
     while let Some(msg) = rx.next().await {
@@ -213,7 +101,6 @@ async fn handle_socket<P: Serialize + DeserializeOwned + Debug + 'static>(
         }
 
         let msg = protocol_from_bytes(&msg_tmp);
-        println!("Received msg: {:#?}", msg);
 
         apply_to_game(user_id.clone(), msg, &conn).await;
     }
