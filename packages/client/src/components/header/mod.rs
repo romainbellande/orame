a2242ecbac27718fd61aa59888ddd231c42c6cf0
Champii1 <contact@champii.io>
2023-11-15 02:54:45 +0100

    WIP

--- packages/client/src/components/header/mod.rs
+++ packages/client/src/components/header/mod.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn Header() -> impl IntoView {
     view! {
-      <div class="navbar flex w-full shadow-sm h-24 bg-white px-4 items-center fixed">
+      <div class="navbar flex w-full shadow-sm h-12 px-4 items-center">
         "Header"
       </div>
     }
